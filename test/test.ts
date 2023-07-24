5af4b8caa02db246245ebf28a25458400e794823
terukazu inoue <terukazu.inoue@codianz.com>
2023-07-24 13:44:51 +0900

    add interface for private tags

--- test/test.ts
+++ test/test.ts
@@ -47,3 +47,18 @@ import * as emv from "../src";
     console.log(`tag 84: ${tag_84.value.to_string()}`);
   }
 }
+
+
+{
+  emv.EmvTags.Instance.addPrivateTags([{tag: "DFDF10", "name": "Test Tag", "template": ["6F"]}])
+  const chunk = emv.data_chunk.create_from_hex_string("6F30DFDF1001FF800205A0810205A0820139830200008410616263642D313233343536372E637274850100860100870200009000");
+  const rapdu = emv.rapdu.create_from_data_chunk(chunk);
+  const tlv = rapdu.get_tlv();
+
+  console.log(`status word: ${rapdu.status_word.sw1sw2.toString(16)}`);
+  console.log(tlv.to_string());
+  const tag_84 = tlv.find_first_child(emv.TLV.tag.create_from_hex_string("84"));
+  if(tag_84.valid){
+    console.log(`tag 84: ${tag_84.value.to_string()}`);
+  }
+}
